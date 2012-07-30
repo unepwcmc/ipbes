9cb78697565d8438c61d38e5b255fdb7ff5de752
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-07-30 09:02:07 +0100

    Show page of assessment - show list of countries instead of multiple

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,7 +63,7 @@ set :deploy_via, :remote_cache
 # The shared area is prepared with 'deploy:setup' and all the shared
 # items are symlinked in when the code is updated.
 # set :local_shared_dirs, %w(public/upload)
-set :local_shared_files, %w(config/database.yml public/sitemap_index.xml.gz public/sitemap1.xml.gz)
+set :local_shared_files, %w(config/database.yml public/sitemap_index.xml.gz public/sitemap1.xml.gz config/initializers/setup_mail.rb)
 
 ## Global Shared Area
 # These are the list of files and directories that you want
