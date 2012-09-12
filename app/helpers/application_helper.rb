3aae0c7a4a07c9a22c80af67a731b4a596600367
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-09-12 08:50:51 +0100

    Markdown helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def markdown(text)
+    return "" if text.nil?
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
     auto_link(markdown.render(text))
   end
