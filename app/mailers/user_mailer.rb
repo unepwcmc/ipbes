8f5b286a41ca19c6269ae0b6371b4989c7215390
Décio Ferreira <decio.ferreira@decioferreira.com>
2012-09-17 09:19:21 +0100

    bug fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,20 +4,20 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
 
-    mail to: user.email, subject: 'IPBES Assessment Catalogue: Welcome'
+    mail to: user.email, subject: 'IPBES Catalogue of Assessments: Welcome'
   end
 
   def approval_request_email(user, admin)
     @user = user
     @url  = "#{APP_CONFIG['url']}/users/#{@user.id}/edit"
 
-    mail to: admin.email, subject: "IPBES Assessment Catalogue: #{@user.name} requests approval" 
+    mail to: admin.email, subject: "IPBES Catalogue of Assessments: #{@user.name} requests approval" 
   end
 
   def approved_email(user)
     @user = user
     @url  = "#{APP_CONFIG['url']}/d/users/sign_in"
 
-    mail to: user.email, subject: 'IPBES Assessment Catalogue: Approved account'
+    mail to: user.email, subject: 'IPBES Catalogue of Assessments: Approved account'
   end
 end
